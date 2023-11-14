0fc06608a9d474a9304e81e1d4e4f8e0c2586aa6
soominnnn <marisol03@naver.com>
2023-11-15 01:52:23 +0900

    :sparkles:feat: constant 오브젝트 추가

---
+++ src/constant/constant.js
@@ -0,0 +1,12 @@
+const CONSTANT = {
+  minDay: 1,
+  ChristmasDay: 25,
+  maxDay: 31,
+  DiscountAmount: 100,
+  starAmount: 5000,
+  treeAmount: 10000,
+  santaAmount: 20000,
+  specialPattern: '/[^0-9ㄱ-ㅎㅏ-ㅣ가-힣\s,/-]/',
+}
+
+export default CONSTANT;
