cdeda907aa9d2f1a90472426b7500e6ee78976f4
soominnnn <marisol03@naver.com>
2023-11-15 01:55:58 +0900

    :package:Chore: ERROR import 및 적용

--- src/model/VisitDay.js
+++ src/model/VisitDay.js
@@ -1,5 +1,6 @@
 import DAY_OF_WEEK from '../constant/DayOfWeek.js';
 import CONSTANT from '../constant/constant.js';
+import ERROR from '../constant/Error.js';
 
 class VisitDay {
   #visitDay;
@@ -13,10 +14,10 @@ class VisitDay {
 
   #validate(userInput) {
     if(!(userInput >= CONSTANT.minDay && userInput <= CONSTANT.maxDay)) {
-      throw new Error("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+      throw new Error(ERROR.nonDayError);
     }
     if(!Number(userInput)) {
-      throw new Error("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+      throw new Error(ERROR.nonDayError);
     }
   }
 
