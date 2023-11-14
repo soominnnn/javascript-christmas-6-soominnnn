c5d0c3dbfe6e4b97f7a00df841485d06249cdde8
soominnnn <marisol03@naver.com>
2023-11-14 23:43:13 +0900

    :sparkles:feat: calculateTotalPrice 기능 구현

---
+++ src/controller/PurchaseController.js
@@ -0,0 +1,25 @@
+import MENU from "../constant/Menu.js";
+
+class PurchaseController {
+  #discountArray;
+  #visitDay;
+  #categoryCountArray;
+
+  constructor(discountArray, visitDay, categoryCountArray) {
+    this.#discountArray = discountArray;
+    this.#visitDay = visitDay;
+    this.#categoryCountArray = categoryCountArray;
+  }
+
+  calculateTotalPrice(items) {
+    const ALL_MENU_ARRAY = MENU.appetizers.concat(MENU.mainCourses, MENU.desserts, MENU.beverages)
+    return items.reduce((total, item, index, array) => {
+      if(index%2 === 0){
+        const FOOD = ALL_MENU_ARRAY.find(menuItem => menuItem.name === item);
+        const quantity = array[index+1];
+        total += FOOD ? FOOD.price * quantity : 0;
+      } 
+      return total;
+    }, 0);
+  }
+}
