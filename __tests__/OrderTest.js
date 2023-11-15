6f6aabe2c637ab562edc7bbfcb4385a33d3deefb
soominnnn <marisol03@naver.com>
2023-11-15 23:31:25 +0900

    🔨Reactor: lint prettier 적용

--- __tests__/OrderTest.js
+++ __tests__/OrderTest.js
@@ -1,22 +1,22 @@
-import Order from '../src/model/Order.js';
-import ERROR from '../src/constant/Error.js';
+import Order from '../src/model/Order';
+import ERROR from '../src/constant/Error';
 
 describe('메뉴 주문 클래스 테스트', () => {
-  test.each([
-    ['해산물파스타-1, 초코아이스크림-2'],
-    ['레드와인-1,스파게티-1'],
-  ])('메뉴판에 없는 메뉴를 입력한 경우 예외를 발생', input => {
-    const RESULT = () => new Order(input);
-    expect(RESULT).toThrow(ERROR.nonOrderError);
-  });
+  test.each([['해산물파스타-1, 초코아이스크림-2'], ['레드와인-1,스파게티-1']])(
+    '메뉴판에 없는 메뉴를 입력한 경우 예외를 발생',
+    input => {
+      const RESULT = () => new Order(input);
+      expect(RESULT).toThrow(ERROR.nonOrderError);
+    },
+  );
 
-  test.each([
-    ['해산물파스타-0,레드와인-1'],
-    ['해산물파스타-1,레드와인-0'],
-  ])('메뉴 개수가 0개 이하인 경우 예외를 발생', input => {
-    const RESULT = () => new Order(input);
-    expect(RESULT).toThrow(ERROR.nonOrderError);
-  });
+  test.each([['해산물파스타-0,레드와인-1'], ['해산물파스타-1,레드와인-0']])(
+    '메뉴 개수가 0개 이하인 경우 예외를 발생',
+    input => {
+      const RESULT = () => new Order(input);
+      expect(RESULT).toThrow(ERROR.nonOrderError);
+    },
+  );
 
   test.each([
     ['해산물파스타-1,해산물파스타-1'],
@@ -29,35 +29,40 @@ describe('메뉴 주문 클래스 테스트', () => {
   test.each([
     ['해산물파스타-1.레드와인-1'],
     ['해산물파스타 1개,레드와인 1개'],
-    ['해산물파스타-1,레드와인01']
+    ['해산물파스타-1,레드와인01'],
   ])('메뉴의 입력 형식이 다를 경우 예외를 발생', input => {
     const RESULT = () => new Order(input);
     expect(RESULT).toThrow(ERROR.nonOrderError);
   });
-  
-  test.each([
-    ['해산물파스타-!'],
-    ['레드와인-1,해산물파스타-@'],
-  ])(',와 - 외에 다른 특수문자가 들어갈 경우 예외를 발생', input => {
-    const RESULT = () => new Order(input);
-    expect(RESULT).toThrow(ERROR.nonOrderError);
-  });
+
+  test.each([['해산물파스타-!'], ['레드와인-1,해산물파스타-@']])(
+    ',와 - 외에 다른 특수문자가 들어갈 경우 예외를 발생',
+    input => {
+      const RESULT = () => new Order(input);
+      expect(RESULT).toThrow(ERROR.nonOrderError);
+    },
+  );
 
   test.each([
     ['RED'],
     ['gotksanfvktmxk-1'],
     ['해산물파스타-one'],
-    ['해산물파스타-1,redWine-2']
+    ['해산물파스타-1,redWine-2'],
   ])('영어가 포함될 경우 예외를 발생', input => {
     const RESULT = () => new Order(input);
     expect(RESULT).toThrow(ERROR.nonOrderError);
   });
 
-  test.each([
-    ['해산물파스타-20'],
-    ['레드와인-2,해산물파스타-30'],
-  ])('메뉴의 개수가 20개 이상일 경우 예외를 발생', input => {
+  test.each([['해산물파스타-20'], ['레드와인-2,해산물파스타-30']])(
+    '메뉴의 개수가 20개 이상일 경우 예외를 발생',
+    input => {
+      const RESULT = () => new Order(input);
+      expect(RESULT).toThrow(ERROR.nonOrderError);
+    },
+  );
+
+  test.each([['레드와인-2']])('음료만 주문한 경우 예외를 발생', input => {
     const RESULT = () => new Order(input);
     expect(RESULT).toThrow(ERROR.nonOrderError);
   });
-})
+});
--- __tests__/VisitDayTest.js
+++ __tests__/VisitDayTest.js
@@ -1,21 +1,20 @@
-import ERROR from "../src/constant/Error";
-import VisitDay from "../src/model/VisitDay";
+import ERROR from '../src/constant/Error';
+import VisitDay from '../src/model/VisitDay';
 
 describe('방문 날짜 클래스 테스트', () => {
-  test.each([
-    ['90'],
-    ['32'],
-  ])('날짜의 범위가 1에서 31 사이가 아닌 경우 예외를 발생', input => {
-    const RESULT = () => new VisitDay(input);
-    expect(RESULT).toThrow(ERROR.nonDayError);
-  });
-  
-  test.each([
-    ['1일'],
-    ['1day'],
-    ['none']
-  ])('입력된 값이 숫자가 아닌 경우 예외를 발생', input => {
-    const RESULT = () => new VisitDay(input);
-    expect(RESULT).toThrow(ERROR.nonDayError);
-  });
-})
+  test.each([['90'], ['32']])(
+    '날짜의 범위가 1에서 31 사이가 아닌 경우 예외를 발생',
+    input => {
+      const RESULT = () => new VisitDay(input);
+      expect(RESULT).toThrow(ERROR.nonDayError);
+    },
+  );
+
+  test.each([['1일'], ['1day'], ['none']])(
+    '입력된 값이 숫자가 아닌 경우 예외를 발생',
+    input => {
+      const RESULT = () => new VisitDay(input);
+      expect(RESULT).toThrow(ERROR.nonDayError);
+    },
+  );
+});
